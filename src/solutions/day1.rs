e61e1a390cc443e71e37c2257e4127cc8f3efdb4
Kevan Hollbach <khollbach@gmail.com>
2021-11-29 19:33:24 -0500

    1-2

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -8,7 +8,10 @@ fn read_input(input: impl BufRead) -> Vec<u32> {
 pub fn main() {
     let nums = read_input(io::stdin().lock());
 
-    let ans = part_1(nums.into_iter());
+    let ans = part_1(nums.iter().copied());
+    println!("{}", ans);
+
+    let ans = part_2(nums.into_iter());
     println!("{}", ans);
 }
 
@@ -18,4 +21,18 @@ fn part_1(nums: impl Iterator<Item=u32>) -> u32 {
 
 fn fuel_required(mass: u32) -> u32 {
     mass / 3 - 2
+}
+
+fn part_2(nums: impl Iterator<Item=u32>) -> u32 {
+    nums.map(fuel_required_recursive).sum()
+}
+
+fn fuel_required_recursive(mass: u32) -> u32 {
+    let fuel = (mass / 3).saturating_sub(2);
+
+    if fuel > 0 {
+        fuel + fuel_required_recursive(fuel)
+    }  else {
+        0
+    }
 }
